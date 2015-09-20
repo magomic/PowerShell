af2e160e05c8ef6f62e8b34cc5ffe5767795c1df
magomic <bounty83@web.de>
2015-09-20 13:19:49 +0200

    GitHub Optimierungen <>{ }</>

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,18 @@
-# 1. Versuch die Historie über die ps sessions hinweg zu persistieren, weniger erfolgreich, historie war zwar geladen (get-history) aber über F7 oder den Pfeiltasten nicht verfügbar
-# Verwendung von Git
-# hochladen von �nderungen
-$MaximumHistoryCount = 100
+# <Verwendung von Github>
+# PowerShell: install-module posh-it
+# Rep klonen in lokales Verzeichnis: 1. in das entpsrechende Verzeichnis wechseln; 2. git clone Pfad_zum_Online_Repository
+# Es gibt den Workflow: WorkingDir -> Index -> Head
+# lokale Änderung wird in Index geschrieben: git add <Dateiname> # Änderung vorschlagen
+# Änderung comitten: git commit -m "Commit Msg für Alle im Index" # Datei ist jetzt im Head (nicht online)
+# ins Online-Repository übertragen: git push origin master # wobei master der branch ist
+# Änderungen vom Online-Repository synchronisieren: git pull
+# weiter Informationen https://rogerdudler.github.io/git-guide/index.de.html
+# </Verwendung von Github>
 
+# 1. Versuch die <Historie>{ über die ps sessions hinweg zu persistieren, weniger erfolgreich, historie war zwar geladen (get-history) aber über F7 oder den Pfeiltasten nicht verfügbar
+$MaximumHistoryCount = 100
 $Shell = $Host.UI.RawUI
 $Shell.WindowTitle = "tell me something..."
-
-#$texfom = "D:\DropBoxen\Dropbox\FOM\LaTeX_FOMstd_original"
-$texori = "D:\DropBoxen\Dropbox\Fom\LaTeX_Leere_Vorlage"
 
 #if (!(Test-Path ~\PowerShell -PathType Container))
 #{   New-Item ~\PowerShell -ItemType Directory
@@ -24,24 +29,26 @@ $texori = "D:\DropBoxen\Dropbox\Fom\LaTeX_Leere_Vorlage"
 #}
 # ENDE Versuch 1
 
-# 2. Versuch (Quelle: Microsoft) klappt, allerdings gehen die Pfeiltasten nicht für vergangene Sessions, h geht
+# 2. Versuch (Quelle: Microsoft) klappt, allerdings gehen die Pfeiltasten nicht fÃ¼r vergangene Sessions, h geht
 sv -name HistoryFile -value "C:\users\erhan\documents\windowspowershell\ethist.ps_history"
 Register-EngineEvent PowerShell.Exiting -Action { Get-History | Export-Clixml $HistoryFile } | out-null
 if (Test-path $HistoryFile) { Import-Clixml $HistoryFile | Add-History }
 # if you don't already have this configured...
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+#} </Historie>
 
 # FUNKTIONEN -----------------------------------------------
-# letzten Schlagzeilen von spon auflisten
+# letzten Schlagzeilen von spon auflisten{
 function Get-spon 
 {
 	echo SpOn-Schlagzeilen
 	#$rssFeed = [xml](New-Object System.Net.WebClient).DownloadString('http://www.spiegel.de/schlagzeilen/tops/index.rss')
 	$rssFeed = [xml](New-Object System.Net.WebClient).DownloadString('http://www.spiegel.de/schlagzeilen/index.rss')
 	$rssFeed.rss.channel.item | Select-Object title -First 5
-}
-# letzten 10 Emails der default inbox auflisten -> funzt nicht
+} #}
+
+# letzten 10 Emails der default inbox auflisten -> funzt nicht{
 Function Get-OutlookInBox 
 {  
  Add-type -assembly "Microsoft.Office.Interop.Outlook" | out-null 
@@ -51,35 +58,43 @@ Function Get-OutlookInBox
  $folder = $namespace.getDefaultFolder($olFolders::olFolderInBox) 
  $folder.items |  
  Select-Object -Property Subject, ReceivedTime, Importance, SenderName 
-}
-# geoeffnete Windows Fenster zeigen
-function windows{ps | where {$_.MainWindowTitle -ne ""}}
-# startet das fom skript für skriptsprachenorientierte programmiersprachen
-function fomskript{ start FOM:\skript*\skriptsprachen.pdf}
-# wechselt in das perl verzeichnis
+}#}
+
+# <geoeffnete Windows Fenster zeigen>{
+function windows{ps | where {$_.MainWindowTitle -ne ""}}#}</geoeffnete Windows Fenster zeigen>
+
+# <startet das fom skript für skriptsprachenorientierte programmiersprachen>{
+function fomskript{ start FOM:\skript*\skriptsprachen.pdf} #}</>
+# <wechselt in das perl verzeichnis>{
 function fomperl{sl Fom:\skript*\perl\
 ls | sort Name
-}
-function fomskripttex{ sl FOM:\skript*\Zusamm*\;start Hauptdatei.tcp }
-# kopier eine leere vorlage als unterordner in das aktuelle verzeichnis
+} # }</>
+# <fomskriptorientiert tcp>{
+function fomskripttex{ sl FOM:\skript*\Zusamm*\;start Hauptdatei.tcp } #}</>
+# kopier eine <leere vorlage> als unterordner in das aktuelle verzeichnis{
 function texhere 
 { 
 	
 		echo LaTeX-Vorlage wird erstellt: $texori.ToString() nach ([string]::Concat($pwd.ToString(),"\conclusion"))
 		copy  $texori.ToString() -Destination ([string]::Concat($pwd.ToString(),"\conclusion")) -Recurse
 		
-}
-# Buch Tex-Datei öffnen
+} #}</leere Vorlage>
+# <BuchWissen> Tex-Datei Öffnen{
 function buch{ sl BUCH:\ ; saps Hauptdatei.tcp  }
 function wissen{sl WISSEN:; saps Hauptdatei.tcp }
+#}</BuchWissen>
 
-# Auflisten aller Com Objekte fuer New-Object 
+# Auflisten <aller Com Objekte> fuer New-Object {
 function Get-ComObjects{
 gci HKLM:\Software\Classes -ea 0| ? {$_.PSChildName -match '^\w+\.\w+$' -and (gp "$($_.PSPath)\CLSID" -ea 0)}
 #| ft PSChildName
-}
+} #}</aller Com Objekte>
 
-# Kurzzugriffe für wichtige Pfade --------------------------------
+# Kurzzugriffe für wichtige Pfade{ --------------------------------
+# <TeX-Pfade>{
+#$texfom = "D:\DropBoxen\Dropbox\FOM\LaTeX_FOMstd_original"
+$texori = "D:\DropBoxen\Dropbox\Fom\LaTeX_Leere_Vorlage"
+# }</TeX-Pfade>
 new-psdrive -name FOM -psprovider FileSystem -root "D:\dropboxen\dropbox\fom\"
 new-psdrive -name PROGS -psprovider filesystem -root "C:\Program Files (x86)\"
 new-psdrive -name PSHOME -PSProvider filesystem -root C:\Users\Erhan\Documents\WindowsPowerShell
@@ -89,19 +104,21 @@ mount -Name CLOUD -PSProvider filesystem -root 'D:\DropBoxen\'
 mount -Name TBOX -PSProvider filesystem -root 'D:\DropBoxen\Mediencenter\'
 New-PSDrive -Name BUCH -PSProvider filesystem -Root TBOX:\buch\Erfahrungen_in_Deutschland\
 mount -Name WISSEN -PSProvider filesystem -root 'F:\Literatur und eigene Arbeiten\Wissenssammlung\'
+# }
 
-
-# ALIASE ----------------------------------------------------
+# <ALIASE>{ ----------------------------------------------------
 set-alias -name np -value notepad.exe
 set-alias -name np++ -value PROGS:\notepad++\notepad++.exe
+# }</ALIASE>
 
-# Ausgabe aller definierten Funktionen
+# <Ausgabe aller definierten Funktionen>{
 echo ''
 echo 'Alle Funktionen:'
 get-childitem function:\ | where {$_.Name.Length -gt 2 } | select {$_.Name} | Format-Wide -AutoSize
 echo ''
 echo 'Alle Laufwerke/Lokationen:'
 get-psdrive | where {$_.Name.Length -gt 2 } | select {$_.Name} | Format-Wide -AutoSize
+# }</Ausgabe aller definierten Funktionen>
 
 # Startpfad festlegen
 sl C:\
